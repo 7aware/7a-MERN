ce7fc76fc452204ab9c4f6f6e229ef6ce9a4904a
7aware <iriley@7aware.com>
2022-10-22 21:52:33 -0600

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,28 @@
+import Alert from './Alert'
+import BigSidebar from './BigSidebar'
+import ChartsContainer from './ChartsContainer'
+import DataFormChartsContainer from './DataFormChartsContainer'
+import FormRow from './FormRow'
+import FormRowSelect from './FormRowSelect'
+import DataFormsContainer from './DataFormsContainer'
+import Loading from './Loading'
+import Logo from './Logo'
+import Navbar from './Navbar'
+import DataFormSearchContainer from './DataFormSearchContainer'
+import SmallSidebar from './SmallSidebar'
+import DataFormStatsContainer from './DataFormStatsContainer'
+export {
+  Logo,
+  FormRow,
+  Alert,
+  Navbar,
+  BigSidebar,
+  SmallSidebar,
+  FormRowSelect,
+  DataFormSearchContainer,
+  DataFormsContainer,
+  DataFormStatsContainer,
+  ChartsContainer,
+  DataFormChartsContainer,
+  Loading,
+}
---
+++ client/src/pages/dashboard/index.js
@@ -0,0 +1,6 @@
+import AddDataForm from './AddDataForm'
+import AllDataForms from './AllDataForms'
+import Profile from './Profile'
+import SharedLayout from './SharedLayout'
+import DataFormStats from './DataFormStats'
+export { AllDataForms, Profile, SharedLayout, DataFormStats, AddDataForm }
